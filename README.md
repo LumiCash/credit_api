b8f1334a291e27d9502b1adb48232f9858356aa8
Brian586 <62236375+Brian586@users.noreply.github.com>
2022-11-28 01:56:15 +0300

    Credit API

---
+++ README.md
@@ -0,0 +1,3 @@
+# Credit API
+
+This is a demo for illustrating Airtime API Integration with Africastalking.
---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require('express');
+
+var bodyParser = require('body-parser');
+
+const app = express();
+
+app.listen(3000);
+
+app.use(express.json())
+
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+// Set your app credentials
+const credentials = {
+    apiKey: 'YOUR API KEY',
+    username: 'USERNAME',
+}
+
+// Initialize the SDK
+const AfricasTalking = require('africastalking')(credentials);
+
+// Get the airtime service
+const airtime = AfricasTalking.AIRTIME;
+
+async function sendAirtime(phone, amount) {
+    const options = {
+        recipients: [{
+            phoneNumber: phone,
+            currencyCode: "KES",
+            amount: amount
+        }]
+    };
+
+    var status = '';
+
+    // That’s it hit send and we’ll take care of the rest
+    await airtime.send(options)
+        .then(response => {
+            console.log(response);
+            status = response.responses[0].status;
+        }).catch(error => {
+            console.log(error);
+
+            status = 'Error: '+error;
+        });
+
+        return status;
+}
+
+app.get('/', (req, res) => {
+    res.sendFile('./index.html', { root: __dirname });
+});
+
+app.post('/myForm',async function(req,res){
+    var phone = req.body.phone;
+    var amount = req.body.amount;
+    var status = '';
+
+    if(phone !== "" && amount != "") {
+        status = await sendAirtime(phone, amount);
+    } 
+
+    res.send("Status: "+status);
+
+    res.end();
+});
